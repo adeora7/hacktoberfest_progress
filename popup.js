2245c12374cb65ecb27b4fab11bfeadb98ca4efd
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-10-03 18:46:22 +0530

    API query added and partial response handling

--- popup.js
+++ popup.js
@@ -1,6 +1,38 @@
 // Query  //
 
-/*https://api.github.com/search/issues?q=author%3Aadeora7+type%3Apr+
-created%3A2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00+is%3Apublic+state%3Amerged+state%3Aopen*/
+function getData(handle){
 
+  var reqUrl = "https://api.github.com/search/issues?q=author%3A"+handle+"+type%3Apr"+
+              "+created%3A2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00+is%3Apublic";
 
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(req.responseText);
+      document.getElementById("prCompleteCount").innerHTML = (data['total_count']>4?"4":data['total_count'])+ "/4";
+      var message = "";
+      switch(data['total_count']){
+        case 0:
+          message = "It is never too late to start.";
+          break;
+        case 1:
+          message = "Still long way to go.";
+          break;
+        case 2:
+          message = "Awesome, you are half way through.";
+          break;
+        case 3:
+          message = "Just one more to go.";
+          break;
+        default:
+          message = "Congratulations, you have completed hacktoberfest 2017.";
+
+      }
+      document.getElementById("message").innerHTML = message;
+    }
+  };
+  req.open("GET", reqUrl, true);
+  // req.setRequestHeader('Cache-Control','max-age=0');
+  req.send();
+
+}
