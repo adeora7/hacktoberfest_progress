7241255973e15cc4a031af47b74f39bf8e005929
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-10-03 12:58:44 +0530

    added basic layout of popup for chrome extension

---
+++ popup.js
@@ -0,0 +1,6 @@
+// Query  //
+
+/*https://api.github.com/search/issues?q=author%3Aadeora7+type%3Apr+
+created%3A2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00+is%3Apublic+state%3Amerged+state%3Aopen*/
+
+
