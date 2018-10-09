b88df873d7ff7d44a28dbc2a2ac77abb155132ae
Ehsan <ehsan@tamhub.com>
2018-10-09 08:55:22 +0300

    Test getMessage function

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
-var assert = require("assert");
-describe("Array", function() {
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const assert = require("assert");
+const getMessage = require("../popup").getMessage;
+const getXHR = require("../popup").getXHR;
+
+const messages = [
+    "It is never too late to start.",
+    "Still long way to go.",
+    "Awesome, you are almost half way through.",
+    "Almost there.",
+    "Just one more to go.",
+    "Congratulations, you have completed hacktoberfest 2018."
+];
+
+function randomTotalCount(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+describe("#getMessage", function () {
+    it("returns the proper message based on number of total count", function() {
+        var totalCount = randomTotalCount(0, 5);
+        assert.equal(getMessage(totalCount), messages[totalCount]);
     });
-  });
 });
